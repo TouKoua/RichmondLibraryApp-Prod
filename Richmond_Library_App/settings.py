34af93206f67b6cfc2a035273e6a3e54fab280df
TouKoua Xiong <70767264+TouKoua@users.noreply.github.com>
2023-11-29 01:03:08 -0600

    added site to hosts

--- Richmond_Library_App/settings.py
+++ Richmond_Library_App/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-g+$lr)aij4^36-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://richmondlibraryapp-prod-production.up.railway.app/', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://richmondlibraryapp-prod-production.up.railway.app']
 
 
 # Application definition
